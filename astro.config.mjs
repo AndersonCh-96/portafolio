1b0354c422c87c0730dd4e6a6ae3ed3a4abca102
AndersonCh-96 <andersonxavier8@hotmail.com>
2025-05-27 00:28:27 -0500

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
